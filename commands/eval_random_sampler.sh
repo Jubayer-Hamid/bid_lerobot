21ce98baa453ec5a2b9166884e437b8f53cfeeab
Jubayer-Hamid <98342897+Jubayer-Hamid@users.noreply.github.com>
2024-08-07 11:14:07 -0700

    Update eval_random_sampler.sh

--- commands/eval_random_sampler.sh
+++ commands/eval_random_sampler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ############################################################################################################
 
-python lerobot/scripts/eval.py -p /iris/u/jubayer/lerobot/vqbet_pusht eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=random --ah_test=1 --temperature=0.5 --noise_level=1.5
+python lerobot/scripts/eval.py -p /path/to/pretrained_strong_model eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=random --ah_test=1 --temperature=0.5 --noise_level=1.5
