43bf5bf9fdb01896af8093a2214e222eb59145e1
Jubayer-Hamid <98342897+Jubayer-Hamid@users.noreply.github.com>
2024-08-07 11:13:04 -0700

    Update eval_bidirectional_sampler.sh

--- commands/eval_bidirectional_sampler.sh
+++ commands/eval_bidirectional_sampler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ############################################################################################################
 
-python lerobot/scripts/eval.py -p /iris/u/jubayer/lerobot/vqbet_pusht eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=bidirectional --reference-policy-name-or-path=/iris/u/jubayer/lerobot/outputs/train/2024-07-23/21-44-50_pusht_vqbet_default/checkpoints/100000/pretrained_model --ah_test=3  --temperature=0.5 --noise_level=0.0
+python lerobot/scripts/eval.py -p /path/to/pretrained_strong_model eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=bidirectional --reference-policy-name-or-path=/path/to/pretrained_weak_model --ah_test=3  --temperature=0.5 --noise_level=0.0
