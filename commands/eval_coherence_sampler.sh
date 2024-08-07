7660eb8e017a3e33d145fad4ee8991dad4f31d3d
Jubayer-Hamid <98342897+Jubayer-Hamid@users.noreply.github.com>
2024-08-06 23:16:45 -0700

    Update eval_coherence_sampler.sh

--- commands/eval_coherence_sampler.sh
+++ commands/eval_coherence_sampler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ############################################################################################################
 
-python lerobot/scripts/eval.py -p /iris/u/jubayer/lerobot/outputs/train/2024-07-18/12-28-37_pusht_vqbet_default/checkpoints/200000/pretrained_model eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=coherence --ah_test=1 --temperature=1.0 --noise_level=0.0
+python lerobot/scripts/eval.py -p /iris/u/jubayer/lerobot/vqbet_pusht eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=coherence --ah_test=1 --temperature=0.5
