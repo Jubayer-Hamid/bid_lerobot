778d9ca11c4fd47fe3552909e62f648231bd04e8
Jubayer-Hamid <98342897+Jubayer-Hamid@users.noreply.github.com>
2024-08-31 20:56:07 -0700

    Update eval_contrastive_sampler.sh

--- commands/eval_contrastive_sampler.sh
+++ commands/eval_contrastive_sampler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ############################################################################################################
 
-python lerobot/scripts/eval.py -p /path/to/pretrained_strong_model eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=contrastive --ah_test=1 --reference-policy-name-or-path=/path/to/pretrained_weak_model --temperature=0.5
+python lerobot/scripts/eval.py -p /lerobot/ckpt/pretrained_strong_model eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=contrastive --ah_test=1 --reference-policy-name-or-path=/lerobot/ckpt/pretrained_weak_model --temperature=0.5
