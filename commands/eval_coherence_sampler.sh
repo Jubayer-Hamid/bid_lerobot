414e1e9fe3289d5f2864115b895c37ea7253c2e1
Jubayer-Hamid <98342897+Jubayer-Hamid@users.noreply.github.com>
2024-08-31 20:56:25 -0700

    Update eval_coherence_sampler.sh

--- commands/eval_coherence_sampler.sh
+++ commands/eval_coherence_sampler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ############################################################################################################
 
-python lerobot/scripts/eval.py -p /path/to/pretrained_strong_model eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=coherence --ah_test=1 --temperature=0.5
+python lerobot/scripts/eval.py -p /lerobot/ckpt/pretrained_strong_model eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=coherence --ah_test=1 --temperature=0.5
