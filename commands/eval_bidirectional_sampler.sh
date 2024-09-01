5b7bc5f504b0cffae993d351e09b2dcf548e6205
Jubayer-Hamid <98342897+Jubayer-Hamid@users.noreply.github.com>
2024-08-31 20:56:53 -0700

    Update eval_bidirectional_sampler.sh

--- commands/eval_bidirectional_sampler.sh
+++ commands/eval_bidirectional_sampler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ############################################################################################################
 
-python lerobot/scripts/eval.py -p /path/to/pretrained_strong_model eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=bidirectional --reference-policy-name-or-path=/path/to/pretrained_weak_model --ah_test=3  --temperature=0.5 --noise_level=0.0
+python lerobot/scripts/eval.py -p /lerobot/ckpt/pretrained_strong_model eval.n_episodes=500 eval.batch_size=50 eval.use_async_envs=true use_amp=true wandb.enable=true --sampler=bidirectional --reference-policy-name-or-path=/lerobot/ckpt/pretrained_weak_model --ah_test=3  --temperature=0.5 --noise_level=0.0
