4b8c67afe84884e1e34adcb54e1557250da2fd1a
Jubayer Ibn Hamid <98342897+Jubayer-Hamid@users.noreply.github.com>
2024-11-26 12:02:38 -0800

    Update multi.py - fix bug in BID and contrastive sampler
    After changing the VQ-BeT model code to allow latent space BID, needed to update the return values of select_action in BID sampling and contrastive sampling.

--- lerobot/common/samplers/multi.py
+++ lerobot/common/samplers/multi.py
@@ -1,13 +1,14 @@
 import torch
-from lerobot.common.samplers.metric import euclidean_distance, coverage_distance
+from lerobot.common.samplers.metric import euclidean_distance, coverage_distance, manhattan_distance, cosine_distance
 
 import pdb
 import ipdb
+import torch.nn.functional as F
 torch.set_printoptions(precision=2, sci_mode=False)
 
 # prior, observation, count, ah_test
 
-def contrastive_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temperature=1.0, num_sample=10, name='contrast', factor=2):
+def contrastive_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temperature=1.0, num_sample=30, name='contrast', factor=2):
     """
     Sample an action by contrasting outputs from strong and weak policies.
 
@@ -30,8 +31,8 @@ def contrastive_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temper
     }
 
     # predict
-    action_strong, action_strong_chunk = strong.select_action(obs_dict_batch, ah_test, temperature)
-    action_weak, action_weak_chunk = weak.select_action(obs_dict_batch, ah_test, temperature)
+    action_strong, action_strong_chunk, _ = strong.select_action(obs_dict_batch, ah_test, temperature)
+    action_weak, action_weak_chunk, _ = weak.select_action(obs_dict_batch, ah_test, temperature)
     action_strong = action_strong.unsqueeze(1)
     action_weak = action_weak.unsqueeze(1)
 
@@ -94,7 +95,9 @@ def contrastive_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temper
         action_dict[key] = action_strong_batch[key][range_tensor, index]
     return action_dict
 
-def bidirectional_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temperature=1.0, num_sample=20, beta=0.90, factor=2):
+
+
+def bidirectional_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temperature=1.0, num_sample=40, beta=1.0, factor=2):
     """
     Sample an action that preserves coherence with a prior and contrast outputs from strong and weak policies.
     Args:
@@ -108,7 +111,12 @@ def bidirectional_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temp
 
     Returns:
         dict: A dictionary of actions sampled using the contrastive approach.
-    """    
+    """ 
+
+    # print(f"temperature: {temperature}")
+    # print(f"beta : {beta}")
+    # print(f"factor: {factor}")
+
     # pre-process
     B, OD = obs_dict['observation.state'].shape
     obs_dict_batch = {
@@ -116,9 +124,10 @@ def bidirectional_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temp
         'observation.image': obs_dict['observation.image'].unsqueeze(1).repeat(1, num_sample, 1, 1, 1).view(B * num_sample, 3, 96, 96)
     }
 
+
     # predict
-    action_strong, action_strong_chunk = strong.select_action(obs_dict_batch, ah_test, temperature)
-    action_weak, action_weak_chunk = weak.select_action(obs_dict_batch, ah_test, temperature)
+    action_strong, action_strong_chunk, _ = strong.select_action(obs_dict_batch, ah_test, temperature)
+    action_weak, action_weak_chunk, _ = weak.select_action(obs_dict_batch, ah_test, temperature)
     action_strong = action_strong.unsqueeze(1)
     action_weak = action_weak.unsqueeze(1)
 
@@ -159,7 +168,9 @@ def bidirectional_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temp
         weights = weights / weights.sum()
 
         # sample selection
-        dist_strong = euclidean_distance(action_strong_batch['action_pred'][:, :, :CH], prior.unsqueeze(1), reduction='none')
+        # dist_strong = euclidean_distance(action_strong_batch['action_pred'][:, :, :CH], prior.unsqueeze(1), reduction='none')
+        # dist_strong = manhattan_distance(action_strong_batch['action_pred'][:, :, :CH], prior.unsqueeze(1), reduction='none')
+        dist_strong = cosine_distance(action_strong_batch['action_pred'][:, :, :CH], prior.unsqueeze(1), reduction='none')
         dist_weighted = dist_strong * weights.view(1, 1, CH)
         dist_strong_sum = dist_weighted.sum(dim=2)
         _, cross_index = dist_strong_sum.sort(descending=False)
@@ -173,7 +184,9 @@ def bidirectional_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temp
         action_strong_batch = action_dict
 
         # sample selection
-        dist_weak = euclidean_distance(action_weak_batch['action_pred'][:, :, :CH], prior.unsqueeze(1), reduction='none')
+        # dist_weak = euclidean_distance(action_weak_batch['action_pred'][:, :, :CH], prior.unsqueeze(1), reduction='none')
+        # dist_weak = manhattan_distance(action_weak_batch['action_pred'][:, :, :CH], prior.unsqueeze(1), reduction='none')
+        dist_weak = cosine_distance(action_weak_batch['action_pred'][:, :, :CH], prior.unsqueeze(1), reduction='none')
         dist_weighted = dist_weak * weights.view(1, 1, CH)
         dist_weak_sum = dist_weighted.sum(dim=2)
         _, cross_index = dist_weak_sum.sort(descending=False)
@@ -189,7 +202,9 @@ def bidirectional_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temp
     # positive samples
     src_expand = action_strong_batch['action_pred'].unsqueeze(1)
     tar_expand =  action_strong_batch['action_pred'].unsqueeze(2)
-    dist_pos = euclidean_distance(src_expand, tar_expand).view(B, num_sample, num_sample)
+    # dist_pos = euclidean_distance(src_expand, tar_expand).view(B, num_sample, num_sample)
+    # dist_pos = manhattan_distance(src_expand, tar_expand).view(B, num_sample, num_sample)
+    dist_pos = cosine_distance(src_expand, tar_expand).view(B, num_sample, num_sample)
 
     topk = num_sample // factor
     values, _ = torch.topk(dist_pos, k=topk, largest=False, dim=-1)
@@ -198,7 +213,9 @@ def bidirectional_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temp
     # negative samples
     src_expand = action_strong_batch['action_pred'].unsqueeze(1)
     tar_expand = action_weak_batch['action_pred'].unsqueeze(2)
-    dist_neg = euclidean_distance(src_expand, tar_expand).view(B, num_sample, num_sample)
+    # dist_neg = euclidean_distance(src_expand, tar_expand).view(B, num_sample, num_sample)
+    # dist_neg = manhattan_distance(src_expand, tar_expand).view(B, num_sample, num_sample)
+    dist_neg = cosine_distance(src_expand, tar_expand).view(B, num_sample, num_sample)
 
     topk = num_sample // factor
     values, _ = torch.topk(dist_neg, k=topk, largest=False, dim=-1)
@@ -215,6 +232,7 @@ def bidirectional_sampler(strong, weak, prior, obs_dict, ah_count, ah_test, temp
         action_dict[key] = action_strong_batch[key][range_tensor, index]
 
     return action_dict
+
 
 
 def bidirectional_sampler_latent(strong_agent, strong, weak, prior, prior_action, obs_dict, ah_count, ah_test, temperature=1.0, num_sample=40, beta=1.0, factor=2):
